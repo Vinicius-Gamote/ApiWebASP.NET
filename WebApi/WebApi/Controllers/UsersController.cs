372eb535883ffd05e903d36a60829145c24ebc8e
Vinicius <v.gamote@dino.com.br>
2021-11-22 16:06:20 -0300

    Project base for mvc api

---
+++ WebApi/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<User> list = new List<User>();
+            list.Add(new User { Id = 1, Name = "Gamotera", Position = "Developer" });
+            list.Add(new User { Id = 2, Name = "Vinicius", Position = "Driver" });
+
+            return View(list);
+        }
+    }
+}
